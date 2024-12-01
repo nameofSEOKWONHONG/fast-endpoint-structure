1dc0baa9dae7f92f334fc7bd019b827c2080aadf
seokwonhong <h20913@gmail.com>
2024-12-02 00:10:23 +0900

    up

---
+++ src/Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Session;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static void AddSessionContext(this IServiceCollection services)
+    {
+        services.AddScoped<ISessionUser, SessionUser>();
+        services.AddScoped<ISessionCurrent, SessionCurrent>();
+        services.AddScoped<ISessionContext, SessionContext>();
+    }
+}
